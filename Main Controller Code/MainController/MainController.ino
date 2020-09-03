2d045cb7d00f045f47c511daf35d234f1e60f523
Juan Vivas <juanvivas@alphrtechnology.co.uk>
2020-09-03 14:23:20 +0100

    RTC and OLED integrated to main controller

--- Main Controller Code/MainController/MainController.ino
+++ Main Controller Code/MainController/MainController.ino
@@ -36,6 +36,7 @@ String readStringIOCtrl;
 void CheckSerialVer3();
 void checkIOCtrlSerial();
 void checkIOCtrlSetOutputTo(int OutputNumber, int State);
+void ShowCurrentTime();
 
 
 void setup()
@@ -102,7 +103,9 @@ void loop()
   readStringIOCtrl = ""; 
   checkIOCtrlInputs();
   checkIOCtrlSerial(); 
-  delay(1000);  
+  delay(1000); 
+
+  ShowCurrentTime();
   
 }
 
@@ -151,5 +154,58 @@ void checkIOCtrlSerial(){
     //readStringIOCtrl = "";   
 }
 
+
+void ShowCurrentTime(){
+ 
+    DateTime now = rtc.now();
+     
+    display.clearDisplay();
+    display.setTextSize(2);
+    display.setCursor(75,0);
+    display.println(now.second(), DEC);
+     
+    display.setTextSize(2);
+    display.setCursor(25,0);
+    display.println(":");
+     
+    display.setTextSize(2);
+    display.setCursor(65,0);
+    display.println(":");
+     
+    display.setTextSize(2);
+    display.setCursor(40,0);
+    display.println(now.minute(), DEC);
+     
+    display.setTextSize(2);
+    display.setCursor(0,0);
+    display.println(now.hour(), DEC);
+     
+    display.setTextSize(2);
+    display.setCursor(0,20);
+    display.println(now.day(), DEC);
+     
+    display.setTextSize(2);
+    display.setCursor(25,20);
+    display.println("-");
+     
+    display.setTextSize(2);
+    display.setCursor(40,20);
+    display.println(now.month(), DEC);
+     
+    display.setTextSize(2);
+    display.setCursor(55,20);
+    display.println("-");
+     
+    display.setTextSize(2);
+    display.setCursor(70,20);
+    display.println(now.year(), DEC);
+     
+    display.setTextSize(2);
+    display.setCursor(0,40);
+    display.print(daysOfTheWeek[now.dayOfTheWeek()]);
+     
+    display.display(); 
+
+}
 
 
