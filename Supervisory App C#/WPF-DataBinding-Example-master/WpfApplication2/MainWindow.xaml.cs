1d7a5b236f20e4ce1275ba5bfd4fd46b336b96fb
Juan Vivas <pleasedontcallmejuan@gmail.com>
2020-09-24 17:16:49 +0100

    Working on the Supervisory application

---
+++ Supervisory App C#/WPF-DataBinding-Example-master/WpfApplication2/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Windows;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System;
+
+namespace WpfApplication2
+{
+    public partial class MainWindow : INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+
+
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+            BoundNumber = 0;
+            ButtonState = !ButtonState;
+        }
+
+        private bool _buttonState;
+
+        public bool ButtonState
+        {
+            get { return _buttonState; }
+            set {
+
+                if (_buttonState != value)
+                {
+                    _buttonState = value;
+                    OnPropertyChanged();
+                    Console.WriteLine("Button pressed " + value.ToString());
+                }
+              
+
+            }
+        }
+
+
+        private int _boundNumber;
+        public int BoundNumber
+        {
+            get { return _boundNumber; }
+            set
+            {
+                if(_boundNumber != value)
+                {
+                    _boundNumber = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
