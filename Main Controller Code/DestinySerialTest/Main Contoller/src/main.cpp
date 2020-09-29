e93e1c2f3243c9e68f15c3367c58ad4fed32c4b1
Juan Vivas <pleasedontcallmejuan@gmail.com>
2020-09-29 22:47:29 +0100

    Tiding up front end

---
+++ Main Controller Code/DestinySerialTest/Main Contoller/src/main.cpp
@@ -0,0 +1,289 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_I2CDevice.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include "RTClib.h"
+#include <SPI.h>
+#include <mySD.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+
+
+
+
+
+
+
+String readString;
+String readStringB;
+String sSerialUSB;
+char nOption;
+
+//String used in the communication with Arduino Aux Board
+String readStringIOCtrl;
+unsigned long nCurrentMillis = millis();
+unsigned long nPreviousMillis ;
+
+
+enum StatesEnum { Start, Initialise, ReadSD, EnablePeripherals, UpdateStatus, updateTFT,  CheckSerialComms };
+StatesEnum CurrentState; 
+
+enum eStateEnum{eCalculateNextTimeCheck,eCheckSD,eUpdateOutputs,eUpdateDisplay,eIdle};
+eStateEnum StateEnum;
+
+
+//Time system is idle
+const long interval = 5*1000;     //in seconds
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void setup() {
+  sSerialUSB="NOthing";
+  Serial.begin(115200);
+  //Serial1.begin();
+  
+  Serial.println("Restarted");
+  Serial.println("");
+  Serial.println("");
+  //Serial_1.print("Serial1 Working");
+  //Serial_1.print('\n');
+
+  //Serial_2.print("Serial2 working");
+  //Serial_2.print('\n');
+  
+
+  
+  Serial.println("Couldn't find RTC");
+  Serial.println(F("SSD1306 allocation failed"));   
+  Serial.println(" SD initialization failed!");
+  Serial.println("Configuration file Present");
+  Serial.println("Schedule.txt:");
+    
+  Serial.println("initialization done.");
+
+
+
+  StateEnum=eCalculateNextTimeCheck;
+  nPreviousMillis=millis();
+
+}
+
+void checkSerial_2();
+void checkSerial_1();
+void CheckSerialVer2();
+void SerialCommandHandller();
+//Serial Communication Handling
+void checkIOCtrlSetOutputTo(int OutputNumber, int State);
+void checkIOCtrlSerial();
+
+void loop() {
+  
+ switch (StateEnum)
+    {
+        
+        case eCalculateNextTimeCheck:
+        {
+            Serial.println("State: eCalculateNextTimeCheck");
+
+             //Show time  
+            
+          
+            Serial.println("1030Mon");
+
+            StateEnum=eCheckSD;
+            break;
+        }
+
+        case eCheckSD:{
+           Serial.println("State: eCheckSD");
+           StateEnum=eUpdateOutputs;
+            break;
+        }
+
+         case eUpdateOutputs:{
+            Serial.println("State: eUpdateOutputs");
+           StateEnum=eUpdateDisplay;
+            break;
+        }
+
+
+         case eUpdateDisplay:{          
+          Serial.println("State: eUpdateDisplay");
+          StateEnum=eIdle;
+          break;
+        }
+
+         case eIdle:{
+          //Serial.println("State: eIdle");
+          nCurrentMillis=millis();
+          if (nCurrentMillis - nPreviousMillis >= interval) {            
+            nPreviousMillis = nCurrentMillis;
+            StateEnum=eCalculateNextTimeCheck;
+            delay(500);
+          }
+
+          CheckSerialVer2();
+          SerialCommandHandller();
+         break;
+
+        default:
+        {
+            // is likely to be an error
+             Serial.println("error opening test.txt");
+            StateEnum=eCalculateNextTimeCheck;
+        }
+    };
+
+
+
+}
+}
+
+#pragma region "Serial Communication Handling"
+void CheckSerialVer2(){  
+    if (!Serial.available()) return;
+    
+    if (Serial.available()) {   
+      sSerialUSB = Serial.readStringUntil('\n');// s1 is String type variable.
+      //delay(10);
+      Serial.print("Received Data = ");
+      Serial.println(sSerialUSB);//display same received Data back in serial monitor. 
+      nOption=sSerialUSB[0];
+    }
+ 
+ }
+
+
+void SerialCommandHandller(){    
+
+  switch ( sSerialUSB[0]) {
+      case 'K':
+        Serial.println("Initialise");
+        sSerialUSB="";
+      break;
+      
+      case 'I':
+        Serial.println("I Idle");
+        sSerialUSB="";
+      break;
+  
+      case 'N':
+        Serial.println("N Update Screen");     
+        sSerialUSB="";
+      break;
+  
+      case 'M':
+        Serial.println("M Check Events");
+        sSerialUSB="";
+      break;
+  
+      case 'O':
+        Serial.println("O Update Outputs");     
+        sSerialUSB="";
+      break;
+  
+      case 'D':
+        Serial.println("D Delete Memory");
+        sSerialUSB="";
+      break;
+  
+      case '0':
+        Serial.println("0 Read Page Number 0_0000");     
+        sSerialUSB="";
+      break;
+  
+      case '1':
+        Serial.println("1 AddEvent  1_1030WED31");     
+        sSerialUSB="";
+      break;
+  
+      case '2':
+        Serial.println("2 Disable Event  2_001");     
+        sSerialUSB="";
+      break;
+  
+      case '3':
+        Serial.println("3 Spare");
+        sSerialUSB="";
+      break;
+  
+      case '4':
+        Serial.println("4 Update Output 4_11 Output 1 ON");     
+        sSerialUSB="";
+      break;
+  
+      case '5':
+        Serial.println("5 Write Status on OLED 5_Hello");     
+        sSerialUSB="";
+      break;
+  
+      case '6':
+        Serial.println("6 Set Time RTC 6_DDMMY_HHmmss"); 
+        
+        sSerialUSB="";
+      break;
+  
+      case '7':
+        Serial.println("7 Read Time");     
+        sSerialUSB="";
+      break;
+  
+      case '8':
+        Serial.println("8 Check Current Event");     
+        sSerialUSB="";
+      break;
+             
+      case '9':
+        Serial.println("9 Spare");     
+        sSerialUSB="";
+      break;
+  
+      case '?':
+        Serial.println("Help");
+        Serial.println("K Initialise");  
+        Serial.println("I Idle");
+        Serial.println("N Update Screen");   
+        Serial.println("M Check Events");
+        Serial.println("O Update Outputs");
+        Serial.println("D Delete Memory");     
+        Serial.println("0 Read Page Number 0_0000");
+        Serial.println("1 AddEvent  1_1030WED31");
+        Serial.println("2 Disable Event  2_001");             
+        Serial.println("3 Spare");
+        Serial.println("4 Update Output 4_11 Output 1 ON");
+        Serial.println("5 Write Status on OLED 5_Hello"); 
+        Serial.println("6 Set Time RTC 6_DDMMY_HHmmss"); 
+        Serial.println("7 Read Time"); 
+        Serial.println("8 Check Current Event");
+        Serial.println("9 Spare");              
+        sSerialUSB="";
+      break;
+       
+      
+      default:
+       //Serial.println("! Not supported");   
+       
+        //sSerialUSB="";
+        break;
+      }  
+}
+
+
+
---
+++ Supervisory App C#/Configuration and Supervisory-Superseeded/Destiny/Destiny/ViewModels/ViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Destiny.Models;
+using System.Collections.ObjectModel;
+
+
+namespace Destiny.ViewModels
+{
+    public class ViewModel
+    {
+        private ObservableCollection<SerialPort> _serialPorts;
+
+        public ObservableCollection<SerialPort> SerialPorts
+        {
+            get { return _serialPorts; }
+            set { _serialPorts = value; }
+        }
+
+
+
+
+
+
+        //private ObservableCollection<SerialPort> _SerialPorts;
+
+        //public ObservableCollection<SerialPort> SerialPorts
+        //{
+        //    get { return _SerialPorts; }
+        //    set { _SerialPorts = value; }
+        //}
+
+
+        public ViewModel() {
+
+            SerialPorts = new ObservableCollection<SerialPort>() { new SerialPort() { PortNumber = 1,Name="Hello" } };
+
+        }
+    }
+}
